ca514376ed129e069a02b914552fbafd14747215
Omar Tarek <omartimh@gmail.com>
2022-06-01 15:27:34 +0200

    Mini-Des

--- inc/server.php
+++ inc/server.php
@@ -1,38 +1,64 @@
 <?php
-
-    include_once 'algorithm.php';
-
+    include_once 'des.php';
     if (isset($_REQUEST)) {
-        if (isset($_REQUEST['submit'])) {
-            $message = $_REQUEST['message'];
-        } else if (isset($_REQUEST['test'])) {
-            $plain_text = '1010 0101';
-            $key = '0010010111';
-            $p10 = '3 5 2 7 4 10 1 9 8 6';
-            $p8 = '6 3 7 4 8 5 1 0';
-            $p4 = '2 4 3 1';
-            $ip = '2 6 3 1 4 8 5 7';
-            $ep = '4 1 2 3 2 3 4 1';
-            
-            # $k = '0 0 1 0 0 1 0 1 1 1';
-            
-            $plain_text = explode(' ', $plain_text);
-            $p10 = explode(' ', $p10);
-            $p8 = explode(' ', $p8);
-            $p4 = explode(' ', $p4);
-            $ip = explode(' ', $ip);
-            $ep = explode(' ', $ep);
+        if (isset($_REQUEST['des'])) {
+            $p10 = explode(' ', '3 5 2 7 4 10 1 9 8 6');
+            $p8 = explode(' ', '6 3 7 4 8 5 10 9');
+            $p4 = explode(' ', '2 4 3 1');
+            $ip = explode(' ', '2 6 3 1 4 8 5 7');
+            $ep = explode(' ', '4 1 2 3 2 3 4 1');
+            $s1 = [
+                [1, 0, 3, 2],
+                [3, 2, 1, 0],
+                [0, 2, 1, 3],
+                [3, 1, 3, 2]
+            ];
+            $s2 = [
+                [0, 1, 2, 3],
+                [2, 0, 1, 3],
+                [3, 0, 1, 0],
+                [2, 1, 0, 3]
+            ];
+            $plain_text = str_split(str_replace(' ', '', '1010 0101'));
+            $key = str_split('0010010111');
+            $des = new Des($p10, $p8, $p4, $ip, $ep, $s1, $s2, $plain_text, $key);
 
-            $key = str_split($key);
-            $key_length = sizeof($key);
-            $k = array_slice($key, 0, $key_length);
-            $k1 = array_slice($key, 0, $key_length / 2);
-            $k2 = array_slice($key, $key_length / 2, $key_length);
+            # Generate Keys
+            $keys = $des->get_keys();
+            $key1 = $keys[0];
+            $key2 = $keys[1];
 
-            $des = new Des($plain_text, $key, $p10, $p8, $p4, $ip, $ep);
-            # $des->get_keys($k1, $k2);
-            $result = $des->premutation($p10, $k);
-            print_r($result);
+            # IP[Plain Text]
+            $pt_ip = $des->premutation($plain_text, $ip);
+            $ipl = array_slice($pt_ip, 0, count($pt_ip) / 2);
+            $ipr = array_slice($pt_ip, count($pt_ip) / 2);
+
+            # Encryption - Round [1]
+            $ipr_ep = $des->premutation($ipr, $ep);
+            $ipr_ep_xor_key1 = $des->xor($ipr_ep, $key1);
+            $ipr_s1 = array_slice($ipr_ep_xor_key1, 0, count($ipr_ep_xor_key1) / 2);
+            $ipr_s2 = array_slice($ipr_ep_xor_key1, count($ipr_ep_xor_key1) / 2);
+            $sbox = $des->get_sbox($ipr_s1, $ipr_s2);
+            $sbox_p4 = $des->premutation($sbox, $p4);
+            $sbox_p4_xor_ipl = $des->xor($sbox_p4, $ipl);
+
+            # Swapping
+            $ipl = $ipr;
+            $ipr = $sbox_p4_xor_ipl;
+
+            # Encryption - Round [2]
+            $ipr_ep = $des->premutation($ipr, $ep);
+            $ipr_ep_xor_key2 = $des->xor($ipr_ep, $key2);
+            $ipr_s1 = array_slice($ipr_ep_xor_key2, 0, count($ipr_ep_xor_key2) / 2);
+            $ipr_s2 = array_slice($ipr_ep_xor_key2, count($ipr_ep_xor_key2) / 2);
+            $sbox = $des->get_sbox($ipr_s1, $ipr_s2);
+            $sbox_p4 = $des->premutation($sbox, $p4);
+            $sbox_p4_xor_ipl = $des->xor($sbox_p4, $ipl);
+            $ipl = $sbox_p4_xor_ipl;
+
+            # Final Permutation IP(-1)
+            $cipher_text = $des->premutation(array_merge($ipl, $ipr), $ip);
+            print_r($cipher_text);
         }
     }
 ?>
--- pages/content.php
+++ pages/content.php
@@ -1,10 +1,3 @@
 <div class="content">
-    <!-- <form action="inc/server.php?submit" method="POST"> -->
-    <form action="inc/server.php?test" method="POST">
-        <span>    
-            <label for="message">Message</label>
-            <input id="message" name="message" type="text" autocomplete="off" required>
-        </span>
-        <button id="x" type="submit">Send</button>
-    </form>
+    <a id="btn" href="inc/server.php/?des">DES</a>
 </div>
